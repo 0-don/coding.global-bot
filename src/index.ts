16ce480d91724d2dc0f7c1edd7ef9b6ded69b592
Don-Cryptus <don.cryptus@gmail.com>
2022-06-18 01:14:34 +0200

    intial config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello World');
+require('dotenv').config();
+import { Client, Intents } from 'discord.js';
+
+// const clientId = process.env.CLIENT_ID!;
+// const guildId = process.env.GUILD_ID!;
+const token = process.env.TOKEN!;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.');
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  }
+});
+
+// Login to Discord with your client's token
+client.login(token);
