be41116bb713035be7c509aa851c209b38d256ce
Don-Cryptus <don.cryptus@gmail.com>
2022-06-20 22:26:20 +0200

    add reaction listener

--- src/commands/roleTemplate.ts
+++ src/commands/roleTemplate.ts
@@ -6,6 +6,29 @@ import {
   MessageEmbed,
 } from 'discord.js';
 
+const exampleEmbed = new MessageEmbed()
+  .setColor('#fd0000')
+  .setTitle('Server Roles')
+  .setAuthor({
+    name: 'discord.global',
+    iconURL:
+      'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png',
+    url: 'https://coding.global',
+  })
+  .setDescription('Select your roles')
+  .setThumbnail(
+    'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png'
+  )
+  .addFields(
+    { name: 'Regular field title', value: 'Some value here' },
+    { name: '\u200B', value: '\u200B' }
+  )
+  .setFooter({
+    text: 'Some footer text here',
+    iconURL:
+      'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png',
+  });
+
 export default {
   data: new SlashCommandBuilder()
     .setName('role-template')
@@ -18,37 +41,17 @@ export default {
       // .setRequired(true)
     ),
   async execute(interaction: CommandInteraction<CacheType>) {
-    console.log();
     const string = interaction.options.getString('input');
-    // console.log(string);
-    const exampleEmbed = new MessageEmbed()
-      .setColor('#fd0000')
-      .setTitle('Server Roles')
-      .setAuthor({
-        name: 'discord.global',
-        iconURL:
-          'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png',
-        url: 'https://coding.global',
-      })
-      .setDescription('Select your roles')
-      .setThumbnail(
-        'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png'
-      )
-      .addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' }
-      )
-      .setFooter({
-        text: 'Some footer text here',
-        iconURL:
-          'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png',
-      });
+
     const message = await interaction.reply({
       embeds: [exampleEmbed],
       // content: 'Select your roles',
       fetchReply: true,
     });
 
-    (message as Message<boolean>).react('😄');
+    (message as Message<boolean>).react(
+      interaction.client.emojis.cache.find((emoji) => emoji.name === 'windows')!
+        .id
+    );
   },
 };
---
+++ src/events/messageReactionAdd.ts
@@ -0,0 +1,36 @@
+import type {
+  MessageReaction,
+  PartialMessageReaction,
+  PartialUser,
+  User,
+} from 'discord.js';
+
+export default {
+  name: 'messageReactionAdd',
+  once: true,
+  async execute(
+    reaction: MessageReaction | PartialMessageReaction,
+    user: User | PartialUser
+  ) {
+    // When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+      // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+      try {
+        await reaction.fetch();
+      } catch (error) {
+        console.error('Something went wrong when fetching the message:', error);
+        // Return as `reaction.message.author` may be undefined/null
+        return;
+      }
+    }
+
+    // Now the message has been cached and is fully available
+    console.log(
+      `${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`
+    );
+    // The reaction is now also fully available and the properties will be reflected accurately:
+    console.log(
+      `${reaction.count} user(s) have given the same reaction to this message!`
+    );
+  },
+};
