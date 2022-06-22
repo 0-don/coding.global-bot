78b3ded00bcada80bb62ffac7d70c51d93a1c24c
Don-Cryptus <don.cryptus@gmail.com>
2022-06-23 00:43:07 +0200

    finished role templates

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,20 +1,9 @@
-import type { Message, MessageEmbedOptions } from 'discord.js';
-
-const exampleEmbed: MessageEmbedOptions = {
-  color: '#00000',
-  description: 'Select your roles',
-};
+import type { Message } from 'discord.js';
+import { editRoleTemplateEmbed } from '../utils/roleTemplate';
 
 export default {
   name: 'messageCreate',
   async execute(msg: Message<boolean>) {
-    console.log(msg.client);
-    if (msg.type === 'REPLY' && msg.reference?.messageId) {
-      const replyMsg = await msg.channel.messages.fetch(
-        msg.reference?.messageId
-      );
-
-      await replyMsg.edit({ embeds: [exampleEmbed] });
-    }
+    await editRoleTemplateEmbed(msg);
   },
 };
--- src/utils/roleTemplate.ts
+++ src/utils/roleTemplate.ts
@@ -1,7 +1,12 @@
-import type { Client, MessageEmbedOptions } from 'discord.js';
-import type { CreateRoleTemplateEmbed, RoleTemplateReaction } from '../types/types';
+import { Client, Message, MessageEmbedOptions, Permissions } from 'discord.js';
+import path from 'path';
+import fs from 'fs';
+import type {
+  CreateRoleTemplateEmbed,
+  RoleTemplateReaction,
+} from '../types/types';
 
-export const roleTemplateEmbed: MessageEmbedOptions = {
+export const roleTemplateExampleEmbed: MessageEmbedOptions = {
   color: '#fd0000',
   title: 'Server Roles',
   description: 'Select your roles',
@@ -14,10 +19,20 @@ export const roleTemplateEmbed: MessageEmbedOptions = {
   },
 };
 
+const roleTemplatePath = path.join(
+  __dirname,
+  '../roleTemplates',
+  'programming-languages.json'
+);
+
 export const createRoleTemplateEmbed = (
   inputTemplate: RoleTemplateReaction,
   client: Client<boolean>
 ): CreateRoleTemplateEmbed => {
+  const roleTemplateEmbed = JSON.parse(
+    JSON.stringify(roleTemplateExampleEmbed)
+  ) as MessageEmbedOptions;
+
   // parse emojis from template
   const emojis = inputTemplate.reactions.map((reaction) => {
     if (!reaction?.emoji) return;
@@ -54,4 +69,55 @@ export const createRoleTemplateEmbed = (
     emojis,
     roleTemplateEmbed,
   };
+};
+
+export const editRoleTemplateEmbed = async (msg: Message<boolean>) => {
+  if (
+    msg.type === 'REPLY' &&
+    msg.reference?.messageId &&
+    msg.content.includes('/editRoleTemplate')
+  ) {
+    const user = await msg.guild?.members.fetch(msg.author.id);
+
+    // check if user has permission to edit the message
+    if (
+      !user?.permissions.has([
+        Permissions.FLAGS.KICK_MEMBERS,
+        Permissions.FLAGS.BAN_MEMBERS,
+      ])
+    ) {
+      return;
+    }
+
+    const json = msg.content
+      .replace('/editRoleTemplate', '')
+      .replaceAll('```', '')
+      .trim();
+    console.log(json);
+    const inputTemplate = JSON.parse(json) as RoleTemplateReaction;
+
+    // parse input template
+    // const inputTemplate = JSON.parse(
+    //   fs.readFileSync(roleTemplatePath, 'utf8')
+    // ) as RoleTemplateReaction;
+
+    // create embeded message or error
+    const { roleTemplateEmbed } = createRoleTemplateEmbed(
+      inputTemplate,
+      msg.client
+    );
+
+    console.log(roleTemplateEmbed);
+
+    if (!roleTemplateEmbed) {
+      return;
+    }
+
+    // get reply message by id
+    const replyMsg = await msg.channel.messages.fetch(msg.reference?.messageId);
+    // edit embeded message
+    await replyMsg.edit({ embeds: [roleTemplateEmbed] });
+
+    await msg.delete();
+  }
 };
