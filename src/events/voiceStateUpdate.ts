93401ab344d08acd1b8371331c2206f24a2c2584
Don-Cryptus <don.cryptus@gmail.com>
2022-06-26 16:11:40 +0200

    finished voice events

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,4 +1,15 @@
-import type { VoiceState } from 'discord.js';
+import type { MessageEmbedOptions, VoiceState } from 'discord.js';
+
+const voiceEmbedExample: MessageEmbedOptions = {
+  color: '#fd0000',
+  description: ``,
+  timestamp: new Date(),
+  footer: {
+    text: 'voice event',
+    icon_url:
+      'https://raw.githubusercontent.com/Don-Cryptus/coding.global-web/main/public/favicon/favicon-96x96.png',
+  },
+};
 
 export default {
   name: 'voiceStateUpdate',
@@ -13,14 +24,27 @@ export default {
 
     if (!voiceEventsChannel || !voiceEventsChannel.isText()) return;
 
-    const oldUser = oldVoiceState.member?.user.toString();
-    const newUser = newVoiceState.member?.user.toString();
+    const userServerName = newVoiceState.member?.user.toString();
+    const userGlobalName = newVoiceState.member?.user.username;
 
     const oldChannel = oldVoiceState.channel?.name;
     const newChannel = newVoiceState.channel?.name;
 
+    const voiceEmbed = JSON.parse(
+      JSON.stringify(voiceEmbedExample)
+    ) as MessageEmbedOptions;
+
+    voiceEmbed.timestamp = new Date();
+    if (!oldChannel) {
+      voiceEmbed.description = `${userServerName} (${userGlobalName}) joined ${newChannel}`;
+    } else if (!newChannel) {
+      voiceEmbed.description = `${userServerName} (${userGlobalName}) left ${oldChannel}`;
+    } else {
+      voiceEmbed.description = `${userServerName} (${userGlobalName}) moved from ${oldChannel} to ${newChannel}`;
+    }
+
     voiceEventsChannel.send({
-      content: `${oldUser} moved from ${oldChannel} to ${newChannel}`,
+      embeds: [voiceEmbed],
       allowedMentions: { users: [] },
     });
   },
