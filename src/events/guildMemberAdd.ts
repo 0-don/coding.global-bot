bb3ef604df4e0eff2a917fcf86a27b5d68ac6f6c
Don-Cryptus <don.cryptus@gmail.com>
2022-06-26 22:29:54 +0200

    another refactor

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,12 +1,12 @@
 import type { GuildMember } from 'discord.js';
 import { joinRole } from '../utils/members/joinRole';
-import { updateUserCount } from '../utils/updateUserCount';
+import { updateUserCount } from '../utils/members/updateUserCount';
 
 export default {
   name: 'guildMemberAdd',
   once: false,
   async execute(member: GuildMember) {
-    await joinRole(member)
+    await joinRole(member);
     await updateUserCount(member);
   },
 };
