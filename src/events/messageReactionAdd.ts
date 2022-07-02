9a117de1247104a2e4b9afaa149c14870f133725
Don-Cryptus <don.cryptus@gmail.com>
2022-07-02 18:03:57 +0200

    role database finished

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -4,7 +4,6 @@ import type {
   PartialUser,
   User,
 } from 'discord.js';
-import { VERIFY_TEMPLATE } from '../utils/constants';
 import { switchRoleFromTemplate } from '../utils/roles/roleTemplateReaction';
 
 export default {
@@ -16,10 +15,6 @@ export default {
   ) {
     // fetch reaction status and roles
     await reaction.fetch();
-
-    // check if reaction is on verify message
-    const verifyTemplate = reaction.message.embeds[0]?.footer?.text;
-    if (verifyTemplate === VERIFY_TEMPLATE) return await reaction.remove();
 
     // add role if not exist when clicked on role template embed
     return await switchRoleFromTemplate(reaction, user, 'add');
