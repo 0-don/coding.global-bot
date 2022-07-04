b47841c36edd236671448f985502a77776718b4a
Don Cryptus <don.cryptus@gmail.com>
2022-07-04 16:10:04 +0200

    bump counter finished

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,16 @@
 import type { Message } from 'discord.js';
+import { bumpCount } from '../utils/messages/bumpCount';
 import { cleanUpVerifyChannel } from '../utils/messages/cleanUpVerifyChannel';
 
 export default {
   name: 'messageCreate',
   once: false,
   async execute(message: Message<boolean>) {
+    console.log(message.interaction);
+
     // remove regular messages in verify channel
     await cleanUpVerifyChannel(message);
+
+    await bumpCount(message);
   },
 };
