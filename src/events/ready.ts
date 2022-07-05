b9121d230fe4747e13143cbea7d5e2046f5d429d
Don-Cryptus <don.cryptus@gmail.com>
2022-07-05 22:31:02 +0200

    remove unused functions

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,15 @@
+import { PrismaClient } from '@prisma/client';
 import { log } from 'console';
 import type { Client } from 'discord.js';
+
+const prisma = new PrismaClient();
 
 export default {
   name: 'ready',
   once: true,
   execute(client: Client<boolean>) {
     log(`Ready! Logged in as ${client.user?.tag}`);
+
+    prisma
   },
 };
