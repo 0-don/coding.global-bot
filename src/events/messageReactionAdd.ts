b72916945f01a812301e20792c84a6f21ba66bea
Don-Cryptus <don.cryptus@gmail.com>
2022-07-07 20:11:39 +0200

    add reaction verification

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -5,6 +5,7 @@ import type {
   User,
 } from 'discord.js';
 import { switchRoleFromTemplate } from '../utils/roles/roleTemplateReaction';
+import { verifyReaction } from '../utils/roles/verifyReaction';
 
 export default {
   name: 'messageReactionAdd',
@@ -15,6 +16,9 @@ export default {
   ) {
     // fetch reaction status and roles
     await reaction.fetch();
+
+    // add verify role on like reaction in verify template
+    await verifyReaction(reaction, user);
 
     // add role if not exist when clicked on role template embed
     return await switchRoleFromTemplate(reaction, user, 'add');
---
+++ src/utils/roles/verifyReaction.ts
@@ -0,0 +1,39 @@
+import type {
+  MessageReaction,
+  PartialMessageReaction,
+  PartialUser,
+  User,
+} from 'discord.js';
+import {
+  MUTE,
+  READ_ONLY,
+  VERIFIED,
+  VERIFY_TEMPLATE,
+  VOICE_ONLY,
+} from '../constants';
+import { getGuildStatusRoles } from './getGuildStatusRoles';
+
+const roles = [VERIFIED, VOICE_ONLY, READ_ONLY, MUTE];
+
+export const verifyReaction = async (
+  reaction: MessageReaction | PartialMessageReaction,
+  user: User | PartialUser
+) => {
+  const isTemplate = reaction.message.embeds[0]?.footer?.text;
+  if (isTemplate !== VERIFY_TEMPLATE) return;
+  if (user.id === reaction.message.author?.id) return;
+  if (user.bot) return;
+
+  const member = await reaction.message.guild?.members.cache
+    .get(user.id)
+    ?.fetch();
+
+  // if status role on user then exit
+
+  if (member?.roles.cache.some((role) => roles.includes(role.name as any)))
+    return;
+
+  let guildStatusRoles = getGuildStatusRoles(reaction.message.guild!);
+
+  member?.roles.add(guildStatusRoles[VERIFIED]!);
+};
