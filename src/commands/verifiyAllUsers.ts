c065c90f72ffc21a4195def2df98e04f88f41c0b
Don-Cryptus <don.cryptus@gmail.com>
2022-07-07 21:28:04 +0200

    remove edit reply

--- src/commands/verifiyAllUsers.ts
+++ src/commands/verifiyAllUsers.ts
@@ -46,7 +46,7 @@ export default {
       // get the user from map collection
       const member = memberCollection[1];
 
-      log(member.user.username);
+      log(`${i}/${memberCount} user: ${member.user.username}`);
 
       // check if user exists in db
       await upsertDbMember(member, 'join');
@@ -69,9 +69,9 @@ export default {
       // verify user
       await member.roles.add(guildStatusRoles[VERIFIED]!.id);
 
-      interaction.editReply({
-        content: `${i}/${memberCount} user: ${member.user.username}`,
-      });
+      // interaction.editReply({
+      //   content: `${i}/${memberCount} user: ${member.user.username}`,
+      // });
       i++;
     }
     return;
