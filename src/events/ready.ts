054bc72de9451bf3db17545ac6f2f40e495a9a47
Don-Cryptus <don.cryptus@gmail.com>
2022-07-09 21:35:50 +0200

    update master

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,17 +1,10 @@
 import { log } from 'console';
 import type { Client } from 'discord.js';
-import { guildMemberCountChart } from '../utils/guilds/guildMemberCountChart';
 
 export default {
   name: 'ready',
   once: true,
   async execute(client: Client<boolean>) {
-    const guilds = (await client.guilds.fetch()).values();
-
-    for (let guild of guilds) {
-      await guildMemberCountChart(guild, client);
-    }
-
     log(`Ready! Logged in as ${client.user?.tag}`);
   },
 };
