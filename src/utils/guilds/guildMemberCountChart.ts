9c82a7217994a325e55f0779c49a3908998fd7d6
Don-Cryptus <don.cryptus@gmail.com>
2022-07-10 17:46:21 +0200

    update

--- src/utils/guilds/guildMemberCountChart.ts
+++ src/utils/guilds/guildMemberCountChart.ts
@@ -55,16 +55,13 @@ export const guildMemberCountChart = async (
     oneDayCount = data[data.length - 1]!.y - data[data.length - 2]!.y;
 
   // create chart data in the range of lookback
-
-  console.log(data);
+  data.slice(
+    // splice only the lookback range if it fits. 2 values minium needed for chart
+    data.length - 2 < lookback ? 0 : lookback * -1
+  );
 
   // create chartjs config
-  const config = chartConfig(
-    data.slice(
-      // splice only the lookback range if it fits. 2 values minium needed for chart
-      -30
-    ) as any
-  );
+  const config = chartConfig(data as any);
 
   // render image from chartjs config as png
   const image = await chartJSNodeCanvas.renderToBuffer(
