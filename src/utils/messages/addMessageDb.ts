cedc84e7558b2060db1a4184e43d89042f10c77a
Don-Cryptus <don.cryptus@gmail.com>
2022-08-03 15:45:42 +0200

    update

--- src/utils/messages/addMessageDb.ts
+++ src/utils/messages/addMessageDb.ts
@@ -7,10 +7,12 @@ export const addMessageDb = async (message: Message<boolean>) => {
   // check if disboard bump command was used
 
   // get info
-  const memberId = message.interaction?.user.id;
+  const memberId = message.member?.user.id;
   const channelId = message.channelId;
   const messageId = message.id;
   const guildId = message.guild?.id;
+
+  console.log(`${memberId} ${channelId} ${messageId} ${guildId}`);
 
   // if info doesnt exist
   if (!guildId || !memberId || message.interaction?.user.bot) return;
