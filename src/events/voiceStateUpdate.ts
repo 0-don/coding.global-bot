ced16c43c5ea71af4401cf380b7cfd19e13036ef
Don-Cryptus <don.cryptus@gmail.com>
2022-08-05 23:06:40 +0200

    update

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,14 +1,18 @@
 import type { Event } from '../types';
 import { logVoiceEvents } from '../utils/voiceEvents/logVoiceEvents';
+// import { logVoiceEventsDb } from '../utils/voiceEvents/logVoiceEventsDb';
 
 export default {
   name: 'voiceStateUpdate',
   once: false,
   async execute(oldVoiceState, newVoiceState) {
     // log voice events in to specific channel
+    console.log('#######');
 
-    // console.log(oldVoiceState, newVoiceState);
+    // save logs to db
+    // await logVoiceEventsDb(oldVoiceState, newVoiceState);
 
+    // internal logging
     await logVoiceEvents(oldVoiceState, newVoiceState);
   },
 } as Event<'voiceStateUpdate'>;
