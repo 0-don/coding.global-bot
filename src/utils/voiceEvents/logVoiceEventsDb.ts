78bf803c9893c7e9f87b88e206d36c1eca88d751
Don-Cryptus <don.cryptus@gmail.com>
2022-08-24 14:56:50 +0200

    refactor voice events

--- src/utils/voiceEvents/logVoiceEventsDb.ts
+++ src/utils/voiceEvents/logVoiceEventsDb.ts
@@ -1,260 +1,116 @@
-// import { PrismaClient } from '@prisma/client';
-// import type { VoiceState } from 'discord.js';
+import { GuildVoiceEvents, PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
+import type { VoiceState } from 'discord.js';
+import { getDaysArray } from '../helpers';
+import utc from 'dayjs/plugin/utc';
 
-// const prisma = new PrismaClient();
+dayjs.extend(utc);
 
-// export const logVoiceEventsDb = async (
-//   oldVoiceState: VoiceState,
-//   newVoiceState: VoiceState
-// ) => {
-//   const memberId = newVoiceState.member?.id ?? oldVoiceState.member?.id;
-//   const guildId = newVoiceState.guild.id ?? oldVoiceState.guild.id;
-//   const channelId = newVoiceState.channel?.id ?? oldVoiceState.channel?.id;
-//   const selfMute = newVoiceState.selfMute ?? oldVoiceState.selfMute;
+const prisma = new PrismaClient();
 
-//   if (!memberId || !guildId || !channelId) return;
+export const logVoiceEventsDb = async (
+  oldVoiceState: VoiceState,
+  newVoiceState: VoiceState
+) => {
+  const memberId = newVoiceState.member?.id ?? oldVoiceState.member?.id;
+  const guildId = newVoiceState.guild.id ?? oldVoiceState.guild.id;
+  const channelId = newVoiceState.channel?.id ?? oldVoiceState.channel?.id;
 
-//   const lastVoiceEvent = await prisma.guildVoiceEvents.findFirst({
-//     where: { memberId },
-//     orderBy: { createdAt: 'desc' },
-//   });
+  if (!memberId || !guildId || !channelId) return;
 
-//   const lastMuteEvent = await prisma.guildMuteEvents.findFirst({
-//     where: { memberId },
-//     orderBy: { createdAt: 'desc' },
-//   });
+  let lastVoiceEvent = await prisma.guildVoiceEvents.findFirst({
+    where: { memberId },
+    orderBy: { id: 'desc' },
+  });
 
-//   // JOINED VOICE CHANNEL
-//   if (!oldVoiceState.channelId && newVoiceState.channelId) {
-//     // IF LAST VOICE EVENT WAS JOIN REMOVE IT BECAUSE OF DUPLICATE
-//     if (lastVoiceEvent?.type === 'JOIN')
-//       await prisma.guildVoiceEvents.delete({
-//         where: { id: lastVoiceEvent.id },
-//       });
+  const data = {
+    memberId,
+    channelId,
+    guildId,
+    lastVoiceEvent,
+  };
 
-//     // IF LAST MUTE EVENT WAS MUTE IT BECAUSE OF DUPLICATE
-//     if (lastMuteEvent?.type === 'MUTE')
-//       await prisma.guildMuteEvents.delete({
-//         where: { id: lastMuteEvent.id },
-//       });
+  // JOINED VOICE CHANNEL
+  if (!oldVoiceState.channelId && newVoiceState.channelId) {
+    console.log('join');
+    // IF LAST VOICE EVENT WAS JOIN REMOVE IT BECAUSE OF DUPLICATE
+    if (lastVoiceEvent?.leave === null)
+      await prisma.guildVoiceEvents.delete({
+        where: { id: lastVoiceEvent.id },
+      });
 
-//     // IF USER JOINS MUTED TO A CHANNEL, LOG MUTE EVENT
-//     if (selfMute)
-//       await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'MUTE',
-//           memberId,
-//           guildId,
-//           channelId,
-//         },
-//       });
+    // CREATE VOICE EVENT AFTER JOIN
+    return await prisma.guildVoiceEvents.create({
+      data: { memberId, channelId, guildId },
+    });
+  }
 
-//     // CREATE VOICE EVENT AFTER JOIN
-//     return await prisma.guildVoiceEvents.create({
-//       data: {
-//         memberId,
-//         channelId,
-//         guildId,
-//         type: 'JOIN',
-//       },
-//     });
-//   }
+  // LEFT VOICE CHANNEL
+  if (oldVoiceState.channelId && !newVoiceState.channelId) {
+    console.log('leave');
+    // CREATE VOICE EVENT AFTER LEAVE
+    if (lastVoiceEvent?.leave === null) {
+      lastVoiceEvent = await daysBetween(data);
+      return await prisma.guildVoiceEvents.update({
+        where: { id: lastVoiceEvent.id },
+        data: { leave: new Date() },
+      });
+    }
 
-//   // LEFT VOICE CHANNEL
-//   if (oldVoiceState.channelId && !newVoiceState.channelId) {
-//     // IF VOICE CHANNEL LEAVE AND LAST EVENT LEAVE EXIT
-//     if (lastVoiceEvent?.type === 'LEAVE') {
-//       // CHECK IF LAST MUTE EVENT WAS MUTE IF YES REMOVE BECAUSE DUPLICATE
-//       if (lastMuteEvent?.type === 'MUTE')
-//         await prisma.guildMuteEvents.delete({
-//           where: { id: lastMuteEvent.id },
-//         });
-//       return;
-//     }
+    return;
+  }
 
-//     // IF VOICE CHANNEL LEAVE AND LAST EVENT MUTE CREATE UNMUTE EVENT
-//     if (lastMuteEvent?.type === 'MUTE')
-//       await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'UNMUTE',
-//           memberId,
-//           guildId,
-//           channelId,
-//         },
-//       });
+  // CHANGED VOICE CHANNEL
+  if (oldVoiceState.channelId !== newVoiceState.channelId) {
+    console.log('change');
+    if (lastVoiceEvent?.leave === null) {
+      lastVoiceEvent = await daysBetween(data);
+      await prisma.guildVoiceEvents.update({
+        where: { id: lastVoiceEvent.id },
+        data: { leave: new Date() },
+      });
+    }
 
-//     // CREATE VOICE EVENT AFTER LEAVE
-//     return (
-//       lastVoiceEvent?.type === 'JOIN' &&
-//       (await prisma.guildVoiceEvents.create({
-//         data: {
-//           memberId,
-//           channelId,
-//           guildId,
-//           type: 'LEAVE',
-//         },
-//       }))
-//     );
-//   }
+    return await prisma.guildVoiceEvents.create({
+      data: { memberId, channelId, guildId },
+    });
+  }
 
-//   // CHANGED VOICE CHANNEL
-//   if (oldVoiceState.channelId !== newVoiceState.channelId) {
-//     if (lastVoiceEvent?.type === 'LEAVE') {
-//       // CREATE JOIN EVENT FOR THE NEW CHANNEL
-//       await prisma.guildVoiceEvents.create({
-//         data: {
-//           memberId,
-//           channelId: newVoiceState.channelId!,
-//           guildId,
-//           type: 'JOIN',
-//         },
-//       });
+  return;
+};
 
-//       if (lastMuteEvent?.type === 'MUTE') {
-//         // IF IN DUPLICATE AND STILL MUTED REMOVE EVENT
-//         await prisma.guildMuteEvents.delete({
-//           where: { id: lastMuteEvent.id },
-//         });
-//         // CREATE NEW MUTE EVENT FOR THE NEW CHANNEL IF STILL MUTED
-//         if (selfMute) {
-//           await prisma.guildMuteEvents.create({
-//             data: {
-//               type: 'MUTE',
-//               memberId,
-//               guildId,
-//               channelId: newVoiceState.channelId!,
-//             },
-//           });
-//         }
-//       }
+async function daysBetween({
+  lastVoiceEvent,
+  memberId,
+  channelId,
+  guildId,
+}: {
+  memberId: string;
+  channelId: string;
+  guildId: string;
+  lastVoiceEvent: GuildVoiceEvents | null;
+}) {
+  if (!lastVoiceEvent?.join) return lastVoiceEvent as GuildVoiceEvents;
 
-//       return;
-//     }
+  const now = new Date();
+  const daysBetween = getDaysArray(lastVoiceEvent.join, now);
+  if (daysBetween.length < 2) return lastVoiceEvent;
 
-//     if (lastVoiceEvent?.type === 'JOIN') {
-//       // CREATE LEAVE EVENT FOR THE OLD CHANNEL
-//       await prisma.guildVoiceEvents.create({
-//         data: {
-//           memberId,
-//           channelId: oldVoiceState.channelId!,
-//           guildId,
-//           type: 'LEAVE',
-//         },
-//       });
-
-//       // CREATE JOIN EVENT FOR THE NEW CHANNEL
-//       await prisma.guildVoiceEvents.create({
-//         data: {
-//           memberId,
-//           channelId: newVoiceState.channelId!,
-//           guildId,
-//           type: 'JOIN',
-//         },
-//       });
-
-//       if (lastMuteEvent?.type === 'MUTE') {
-//         // IF USER WAS MUTED BEFORE CHANNEL CHANGE CLOSE IT
-//         await prisma.guildMuteEvents.create({
-//           data: {
-//             type: 'UNMUTE',
-//             memberId,
-//             guildId,
-//             channelId: oldVoiceState.channelId!,
-//           },
-//         });
-//         // IF USER STILL MUTED AFTER SERVE CHANGE ADD MUTE EVENT
-//         if (selfMute) {
-//           await prisma.guildMuteEvents.create({
-//             data: {
-//               type: 'MUTE',
-//               memberId,
-//               guildId,
-//               channelId: newVoiceState.channelId!,
-//             },
-//           });
-//         }
-//       }
-//       return;
-//     }
-
-//     // IF NO MUTE LOGS EXIST CREATE MUTE LOG OR LAST LOG UNMUTE AND CURRENTLY MUTE
-//     if (!lastMuteEvent || (lastMuteEvent.type === 'UNMUTE' && selfMute)) {
-//       await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'MUTE',
-//           memberId,
-//           guildId,
-//           channelId: newVoiceState.channelId!,
-//         },
-//       });
-//     }
-
-//     // DELETE MUTE EVENT IF SOMEHOW OLD MUTE EXIST ON CHANNEL CHANGE
-//     if (lastMuteEvent?.type === 'MUTE') {
-//       await prisma.guildMuteEvents.delete({
-//         where: { id: lastMuteEvent.id },
-//       });
-//       // IF MUTED ON CHANNEL CHANGE CREATE NEW MUTE EVENT
-//       if (selfMute) {
-//         await prisma.guildMuteEvents.create({
-//           data: {
-//             type: 'MUTE',
-//             memberId,
-//             guildId,
-//             channelId: newVoiceState.channelId!,
-//           },
-//         });
-//       }
-//     }
-
-//     // CREATE VOICE IF NO EVENTS EXIST
-//     return await prisma.guildVoiceEvents.create({
-//       data: {
-//         memberId,
-//         channelId: newVoiceState.channelId!,
-//         guildId,
-//         type: 'JOIN',
-//       },
-//     });
-//   }
-
-//   // MUTE STATE CHANGED
-//   if (oldVoiceState.channelId === newVoiceState.channelId) {
-//     if (lastMuteEvent?.type === 'MUTE' && !selfMute) {
-//       // IF LAST MUTE LOG WAS MUTE AND NOW UNMUTE ADD LOG
-//       return await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'UNMUTE',
-//           memberId,
-//           guildId,
-//           channelId,
-//         },
-//       });
-//     }
-
-//     if (lastMuteEvent?.type === 'UNMUTE' && selfMute) {
-//       // IF LAST MUTE LOG WAS UNMUTE AND NOW MUTE ADD LOG
-//       return await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'MUTE',
-//           memberId,
-//           guildId,
-//           channelId,
-//         },
-//       });
-//     }
-
-//     // IF NO MUTE LOGS EXIST CREATE MUTE LOG
-//     if (selfMute) {
-//       await prisma.guildMuteEvents.create({
-//         data: {
-//           type: 'MUTE',
-//           memberId,
-//           guildId,
-//           channelId,
-//         },
-//       });
-//     }
-//   }
-//   return;
-// };
+  for (let i = 1; i < daysBetween.length; i++) {
+    await prisma.guildVoiceEvents.update({
+      where: { id: lastVoiceEvent.id },
+      data: {
+        leave: dayjs(lastVoiceEvent.join).utc().endOf('day').toDate(),
+      },
+    });
+    lastVoiceEvent = await prisma.guildVoiceEvents.create({
+      data: {
+        memberId,
+        channelId,
+        guildId,
+        join: dayjs(daysBetween[i]).utc().startOf('day').toDate(),
+      },
+    });
+  }
+  return lastVoiceEvent;
+}
