f38810fbba1002e54a97de8d5fdbaf1e4d0a0113
Don-Cryptus <don.cryptus@gmail.com>
2022-11-20 14:11:08 +0100

    test

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -15,9 +15,10 @@ export default {
     // update user count channel
     await updateMemberCount(member);
 
+    console.log('test', member.pending);
     // rules not yet accepted
     if (member.pending) return;
-
+    console.log('test2', member.pending);
     // if first time user give him status role
     await joinRole(member);
   },
