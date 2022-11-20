ee41d230bc7641cb853f556bddc9ea318d9c4cd1
Don-Cryptus <don.cryptus@gmail.com>
2022-11-20 13:51:51 +0100

    test

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -9,13 +9,12 @@ export default {
   name: 'guildMemberAdd',
   once: false,
   async execute(member: GuildMember) {
+    console.log(member.pending);
     // create or update user with his roles
     await upsertDbMember(member, 'join');
 
     // update user count channel
     await updateMemberCount(member);
-
-    console.log(member.pending);
 
     // rules not yet accepted
     if (member.pending) return;
