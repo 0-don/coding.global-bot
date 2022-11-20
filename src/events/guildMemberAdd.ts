4fa2451c3dac29b1e502fb2f018d9ddc5ea3524f
Don-Cryptus <don.cryptus@gmail.com>
2022-11-20 13:49:49 +0100

    test

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -15,6 +15,8 @@ export default {
     // update user count channel
     await updateMemberCount(member);
 
+    console.log(member.pending);
+
     // rules not yet accepted
     if (member.pending) return;
 
