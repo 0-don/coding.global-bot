8040ac5bee2dcde52b39137044528c4f9841de07
Don-Cryptus <don.cryptus@gmail.com>
2023-01-21 23:31:54 +0100

    test

--- src/commands/deleteMessages.ts
+++ src/commands/deleteMessages.ts
@@ -1,54 +1,55 @@
-import {SlashCommandBuilder} from '@discordjs/builders';
-import {PermissionFlagsBits} from 'discord-api-types/v9';
-import {CacheType, ChannelType, CommandInteraction} from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { PermissionFlagsBits } from 'discord-api-types/v9';
+import { CacheType, ChannelType, CommandInteraction } from 'discord.js';
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('delete-messages')
-        .setDescription('Deletes messages from a channel')
-        .addStringOption((option) =>
-            option
-                .setName('amount')
-                .setDescription('Delete message History')
-                .setRequired(true)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
-    async execute(interaction: CommandInteraction<CacheType>) {
-        // get user from slash command input
-        const channel = interaction.channel;
-        if (channel == null) return;
-        // get how many days to delete
-        const amount = interaction.options.get('amount')?.value as number;
+  data: new SlashCommandBuilder()
+    .setName('delete-messages')
+    .setDescription('Deletes messages from a channel')
+    .addStringOption((option) =>
+      option
+        .setName('amount')
+        .setDescription('Delete message History')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+  async execute(interaction: CommandInteraction<CacheType>) {
+    // get user from slash command input
+    const channel = interaction.channel;
+    if (channel == null) return;
+    // get how many days to delete
+    const amount = interaction.options.get('amount')?.value as number;
 
-        const guildId = interaction.guild?.id;
+    const guildId = interaction.guild?.id;
 
-        if (!guildId) return;
+    if (!guildId) return;
 
-        // deferReply if it takes longer then usual
-        await interaction.deferReply({ephemeral: true});
+    // deferReply if it takes longer then usual
+    await interaction.deferReply({ ephemeral: true });
 
-        // loop over all channels
+    // loop over all channels
 
-        try {
-            // if channel is not a text channel, continue
-            if (channel.type !== ChannelType.GuildText) return;
-            for (let i = 0; i < Math.floor(amount / 100); i++) {
-                let messagesToDelete = amount - i * 100;
+    try {
+      // if channel is not a text channel, continue
+      if (channel.type !== ChannelType.GuildText) return;
+      for (let i = 0; i < Math.floor(amount / 100); i++) {
+        let messagesToDelete = amount - i * 100;
 
-                channel.messages.cache.clear()
-                await channel.messages.fetch({limit: messagesToDelete > 100 ? 100 : messagesToDelete});
-                const messages = channel.messages.cache.values();
+        channel.messages.cache.clear();
+        await channel.messages.fetch({
+          limit: messagesToDelete > 100 ? 100 : messagesToDelete,
+        });
+        const messages = channel.messages.cache.values();
 
-                let promises = [];
-                for (let message of messages) {
-                    promises.push(message.delete());
-                }
-                await Promise.all(promises);
-            }
-        } catch (_) {
+        let promises = [];
+        for (let message of messages) {
+          promises.push(message.delete());
         }
+        await Promise.all(promises);
+      }
+    } catch (_) {}
 
-        // notify that messages were deleted
-        interaction.editReply({content: 'messages are deleted'});
-    },
+    // notify that messages were deleted
+    interaction.editReply({ content: 'messages are deleted' });
+  },
 };
