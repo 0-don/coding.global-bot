1124925a88189ec18c075b81c73435d763e7673a
Don-Cryptus <don.cryptus@gmail.com>
2023-03-03 19:59:50 +0100

    update

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,4 +1,5 @@
 import type { Event } from '../types/index.js';
+import { replyChatGPT } from '../utils/chatgpt/replyChatGPT.js';
 import { addMessageDb } from '../utils/messages/addMessageDb.js';
 import { checkWarnings } from '../utils/messages/checkWarnings.js';
 import { cleanUpVerifyChannel } from '../utils/messages/cleanUpVerifyChannel.js';
@@ -13,6 +14,9 @@ export default {
 
     //delete messages with links
     await checkWarnings(message);
+
+    //reply to messages with /ai
+    await replyChatGPT(message);
 
     // translate message
     await translateReply(message);
