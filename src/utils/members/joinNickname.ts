eafd6826d6c8fbd9cbfa4785723634144730cef3
Don-Cryptus <don.cryptus@gmail.com>
2023-03-27 19:01:26 +0200

    asd

--- src/utils/members/joinNickname.ts
+++ src/utils/members/joinNickname.ts
@@ -1,6 +1,5 @@
 import type { GuildMember, PartialGuildMember, VoiceState } from 'discord.js';
 import { prisma } from '../../prisma.js';
-import { updateUserVoiceState } from '../voice/updateUserVoiceState.js';
 
 export const joinSettings = async (
   member: GuildMember | PartialGuildMember,
@@ -22,22 +21,13 @@ export const joinSettings = async (
     if (guildMember.nickname && guildMember.nickname !== member.nickname) {
       await member.setNickname(guildMember.nickname);
     }
-    console.log(
-      'guildMember.muted',
-      guildMember.muted,
-      'member.voice.serverMute',
-      member.voice.serverMute
-    );
-    if (voiceState?.channelId) {
-      if (member.voice.serverMute !== guildMember.muted) {
-        // await updateUserVoiceState(voiceState);
-        await member.voice.setMute(guildMember.muted);
-      }
 
-      if (member.voice.serverDeaf !== guildMember.deafened) {
-        // await updateUserVoiceState(voiceState);
+    if (voiceState?.channelId) {
+      if (member.voice.serverMute !== guildMember.muted)
+        await member.voice.setMute(guildMember.muted);
+
+      if (member.voice.serverDeaf !== guildMember.deafened)
         await member.voice.setDeaf(guildMember.deafened);
-      }
     }
   }
 };
