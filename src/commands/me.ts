e2fb30367332da2b12e0c7a587f0b71b7416f554
Don-Cryptus <don.cryptus@gmail.com>
2023-03-27 17:29:48 +0200

    asd

--- src/commands/me.ts
+++ src/commands/me.ts
@@ -1,8 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import type { CacheType,CommandInteraction,TextChannel } from 'discord.js';
+import type { CacheType, CommandInteraction, TextChannel } from 'discord.js';
 import { BOT_CHANNEL } from '../utils/constants.js';
 import { userStatsEmbed } from '../utils/stats/userStatsEmbed.js';
-
 
 export default {
   data: new SlashCommandBuilder()
@@ -10,27 +9,24 @@ export default {
     .setDescription('Get your stats'),
   async execute(interaction: CommandInteraction<CacheType>) {
     // get text channel
-    console.log(1);
 
     // deferReply if it takes longer then usual
-    // await interaction.deferReply();
-    
-    const channel = (await interaction.channel?.fetch()) as TextChannel;
-    console.log(2);
+    interaction.deferReply();
 
-    console.log(3);
+    const channel = (await interaction.channel?.fetch()) as TextChannel;
+
     // if not bot channel, return
     if (channel.name !== BOT_CHANNEL)
       return interaction.editReply(
         'Please use this command in the bot channel'
       );
-    console.log(4);
+
     const embed = await userStatsEmbed(interaction);
-    console.log(5);
-    if (typeof embed === 'string') return interaction.reply(embed);
-    console.log(6);
+
+    if (typeof embed === 'string') return interaction.editReply(embed);
+
     // return embed with chart img
-    return interaction.reply({
+    return interaction.editReply({
       embeds: [embed],
       allowedMentions: { users: [] },
     });
