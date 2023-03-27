ec180d1c54d30f126f44e58e8fdde0f1ce141c44
Don-Cryptus <don.cryptus@gmail.com>
2023-03-27 21:58:15 +0200

    tet

--- src/commands/ai.ts
+++ src/commands/ai.ts
@@ -15,16 +15,19 @@ export default {
     ),
   async execute(interaction: CommandInteraction<CacheType>) {
     // deferReply if it takes longer then usual
-    console.log('test');
+    console.log(1);
     await interaction.deferReply();
-
+    console.log(2);
     const user = interaction.user;
+    console.log(3);
 
     const text = interaction.options.get('text')?.value as string;
+    console.log(4);
 
     const content = await askChatGPT({ interaction, user, text });
-
+    console.log(5);
     if (!content) return await interaction.editReply('User not Found');
+    console.log(6);
     // send success message
 
     return await chunkedSend({ content, interaction });
