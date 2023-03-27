f4168d2b3d5625172f559095f0031213a4eccc4b
Don-Cryptus <don.cryptus@gmail.com>
2023-03-28 00:37:53 +0200

    asd

--- src/commands/me.ts
+++ src/commands/me.ts
@@ -1,9 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import type {
-  CacheType,
-  ChatInputCommandInteraction,
-  TextChannel,
-} from 'discord.js';
+import type { CacheType, CommandInteraction, TextChannel } from 'discord.js';
 import { BOT_CHANNEL } from '../utils/constants.js';
 import { userStatsEmbed } from '../utils/stats/userStatsEmbed.js';
 
@@ -11,17 +7,17 @@ export default {
   data: new SlashCommandBuilder()
     .setName('me')
     .setDescription('Get your stats'),
-  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
+  async execute(interaction: CommandInteraction<CacheType>) {
     // get text channel
 
     // deferReply if it takes longer then usual
-    await interaction.reply('asd');
+    await interaction.deferReply();
 
     const channel = (await interaction.channel?.fetch()) as TextChannel;
 
     // if not bot channel, return
     if (channel.name !== BOT_CHANNEL)
-      return interaction.editReply(
+      return await interaction.editReply(
         'Please use this command in the bot channel'
       );
 
