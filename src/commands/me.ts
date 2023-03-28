cc7ece0ee20011a4fcaf8de5ec8c2017e923de27
Don-Cryptus <don.cryptus@gmail.com>
2023-03-28 02:03:24 +0200

    asd

--- src/commands/me.ts
+++ src/commands/me.ts
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import type { CacheType, CommandInteraction, TextChannel } from 'discord.js';
 import { BOT_CHANNEL } from '../utils/constants.js';
-import { sleep } from '../utils/helpers.js';
 import { userStatsEmbed } from '../utils/stats/userStatsEmbed.js';
 
 export default {
@@ -10,9 +9,7 @@ export default {
     .setDescription('Get your stats'),
   async execute(interaction: CommandInteraction<CacheType>) {
     // get text channel
-
-    await sleep(100);
-
+    console.log('test');
     // deferReply if it takes longer then usual
     await interaction.deferReply();
 
