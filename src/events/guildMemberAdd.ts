326acc9ad9a595125d2b8136f077eae95da5d241
Don-Cryptus <don.cryptus@gmail.com>
2023-08-01 23:33:14 +0200

    test

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,29 +1,29 @@
-import type { GuildMember } from 'discord.js';
-import type { Event } from '../types/index.js';
-import { joinSettings } from '../utils/members/joinNickname.js';
-import { joinRole } from '../utils/members/joinRole.js';
-import { updateMemberCount } from '../utils/members/updateMemberCount.js';
-import { upsertDbMember } from '../utils/members/upsertDbMember.js';
-import { logJoinLeaveEvents } from '../utils/members/logJoinLeaveEvents.js';
+import type { GuildMember } from "discord.js";
+import type { Event } from "../types/index.js";
+import { joinSettings } from "../utils/members/joinNickname.js";
+import { joinRole } from "../utils/members/joinRole.js";
+import { logJoinLeaveEvents } from "../utils/members/logJoinLeaveEvents.js";
+import { updateMemberCount } from "../utils/members/updateMemberCount.js";
+import { upsertDbMember } from "../utils/members/upsertDbMember.js";
 
 export default {
-  name: 'guildMemberAdd',
+  name: "guildMemberAdd",
   once: false,
   async execute(member: GuildMember) {
+    await logJoinLeaveEvents(member, "join");
+
     // create or update user with his roles
-    await upsertDbMember(member, 'join');
+    await upsertDbMember(member, "join");
 
     // rules not yet accepted
     if (member.pending) return;
 
     // if first time user give him status role
-    await joinRole(member, 'unverified');
+    await joinRole(member, "unverified");
 
     // update user count channel
     await updateMemberCount(member);
 
-    await logJoinLeaveEvents(member, 'join')
-
     await joinSettings(member);
   },
-} as Event<'guildMemberAdd'>;
+} as Event<"guildMemberAdd">;
