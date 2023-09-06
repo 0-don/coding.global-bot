f530ebc5a2ec7feb93e51eeaebff87932a62e3b5
Don-Cryptus <don.cryptus@gmail.com>
2023-09-06 21:57:49 +0200

    easy

--- src/utils/chatgpt/replyChatGPT.ts
+++ src/utils/chatgpt/replyChatGPT.ts
@@ -23,14 +23,14 @@ export const replyChatGPT = async (message: Message<boolean>) => {
     const channels = await channel.guild.channels.fetch();
     const botChannel = channels.find((channel) => channel?.name === 'bot');
 
-    if (
-      channel.name === 'general' &&
-      !guildMember?.roles.cache.some((role) =>
-        memberRoles.includes(role.name as any)
-      )
-    ) {
-      return channel.send(`use this command in ${botChannel?.toString()}`);
-    }
+    // if (
+    //   channel.name === 'general' &&
+    //   !guildMember?.roles.cache.some((role) =>
+    //     memberRoles.includes(role.name as any)
+    //   )
+    // ) {
+    //   return channel.send(`use this command in ${botChannel?.toString()}`);
+    // }
 
     const messages = await fetchMessages(channel, 500);
     const replyMsgIndex = messages.findIndex((msg) => msg.id === replyMsg.id);
