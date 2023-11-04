142234bff8750783484a518f305760c4666cd18f
don.cryptus <don.cryptus@gmail.com>
2023-11-04 21:53:25 +0100

    eas

--- src/events/VoiceStateUpdate.ts
+++ src/events/VoiceStateUpdate.ts
@@ -3,10 +3,7 @@ import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
 import { joinSettings } from "../modules/members/joinNickname.js";
 import { moveMemberToChannel } from "../modules/members/moveMemberToChannel.js";
-import { closeDeadVoiceEvents } from "../modules/voice/closeDeadVoiceEvents.js";
-import { logVoiceEvents } from "../modules/voice/logVoiceEvents.js";
-import { logVoiceEventsDb } from "../modules/voice/logVoiceEventsDb.js";
-import { updateUserVoiceState } from "../modules/voice/updateUserVoiceState.js";
+import { VoiceModule } from "../modules/voice/Voice.module.js";
 import { prisma } from "../prisma.js";
 
 @Discord()
@@ -31,17 +28,17 @@ export class VoiceStateUpdate {
     if (!oldVoiceState.channelId)
       await joinSettings(newVoiceState.member as GuildMember, newVoiceState);
 
-    await updateUserVoiceState(newVoiceState);
+    await VoiceModule.updateUserVoiceState(newVoiceState);
 
     if (!oldVoiceState.channelId && newVoiceState.channelId)
       moveMemberToChannel(newVoiceState.member as GuildMember);
 
     // save logs to db
-    await logVoiceEventsDb(oldVoiceState, newVoiceState);
+    await VoiceModule.logVoiceEventsDb(oldVoiceState, newVoiceState);
 
     // internal logging
-    await logVoiceEvents(oldVoiceState, newVoiceState);
+    await VoiceModule.logVoiceEvents(oldVoiceState, newVoiceState);
 
-    await closeDeadVoiceEvents();
+    await VoiceModule.closeDeadVoiceEvents();
   }
 }
---
+++ src/modules/voice/Voice.module.ts
@@ -0,0 +1,192 @@
+import { GuildVoiceEvents } from "@prisma/client";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import { APIEmbed, TextChannel, VoiceState } from "discord.js";
+import { prisma } from "../../prisma.js";
+import { VOICE_EVENT_CHANNEL, simpleEmbedExample } from "../constants.js";
+import { getDaysArray } from "../helpers.js";
+dayjs.extend(utc);
+
+export class VoiceModule {
+  static async closeDeadVoiceEvents() {
+    // roll dice with if statemen 1/10 chance to run
+    if (Math.floor(Math.random() * 10) === 0) return;
+
+    const allOpenVoiceEvents = await prisma.guildVoiceEvents.findMany({
+      where: { leave: null },
+    });
+
+    // dayjs if event is older than 10 days delete it
+    allOpenVoiceEvents.forEach(async (event) => {
+      if (dayjs().diff(dayjs(event.join), "days") > 10) {
+        await prisma.guildVoiceEvents.delete({ where: { id: event.id } });
+      }
+    });
+  }
+
+  static async logVoiceEvents(
+    oldVoiceState: VoiceState,
+    newVoiceState: VoiceState,
+  ) {
+    try {
+      // if mute, deafen, stream etc. => exit
+      if (oldVoiceState.channelId === newVoiceState.channelId) return;
+
+      // get voice channel by name
+      const voiceEventsChannel = oldVoiceState.guild.channels.cache.find(
+        ({ name }) => name === VOICE_EVENT_CHANNEL,
+      );
+
+      // check if voice channel exists and it is voice channel
+      if (!voiceEventsChannel || !voiceEventsChannel.isTextBased()) return;
+
+      const userServerName = newVoiceState.member?.user.toString();
+      const userGlobalName = newVoiceState.member?.user.username;
+
+      const oldChannel = oldVoiceState.channel?.name;
+      const newChannel = newVoiceState.channel?.name;
+
+      // copy paste embed so it doesnt get overwritten
+      const voiceEmbed = JSON.parse(
+        JSON.stringify(simpleEmbedExample),
+      ) as APIEmbed;
+
+      // create embed based on event
+      voiceEmbed.timestamp = new Date().toISOString();
+      if (!oldChannel) {
+        voiceEmbed.description = `${userServerName} (${userGlobalName}) joined ${newChannel}`;
+      } else if (!newChannel) {
+        voiceEmbed.description = `${userServerName} (${userGlobalName}) left ${oldChannel}`;
+      } else {
+        voiceEmbed.description = `${userServerName} (${userGlobalName}) moved from ${oldChannel} to ${newChannel}`;
+      }
+
+      // send embed event to voice channel
+      (voiceEventsChannel as TextChannel).send({
+        embeds: [voiceEmbed],
+        allowedMentions: { users: [] },
+      });
+    } catch (_) {}
+  }
+
+  static async logVoiceEventsDb(
+    oldVoiceState: VoiceState,
+    newVoiceState: VoiceState,
+  ) {
+    const memberId = newVoiceState.member?.id ?? oldVoiceState.member?.id;
+    const guildId = newVoiceState.guild.id ?? oldVoiceState.guild.id;
+    const channelId = newVoiceState.channel?.id ?? oldVoiceState.channel?.id;
+
+    if (!memberId || !guildId || !channelId) return;
+
+    let lastVoiceEvent = await prisma.guildVoiceEvents.findFirst({
+      where: { memberId },
+      orderBy: { id: "desc" },
+    });
+
+    const data = {
+      memberId,
+      channelId,
+      guildId,
+      lastVoiceEvent,
+    };
+
+    // JOINED VOICE CHANNEL
+    if (!oldVoiceState.channelId && newVoiceState.channelId) {
+      // IF LAST VOICE EVENT WAS JOIN REMOVE IT BECAUSE OF DUPLICATE
+      if (lastVoiceEvent?.leave === null)
+        await prisma.guildVoiceEvents.delete({
+          where: { id: lastVoiceEvent.id },
+        });
+
+      // CREATE VOICE EVENT AFTER JOIN
+      return await prisma.guildVoiceEvents.create({
+        data: { memberId, channelId, guildId },
+      });
+    }
+
+    // LEFT VOICE CHANNEL
+    if (oldVoiceState.channelId && !newVoiceState.channelId) {
+      // CREATE VOICE EVENT AFTER LEAVE
+      if (lastVoiceEvent?.leave === null) {
+        lastVoiceEvent = await daysBetween(data);
+        return await prisma.guildVoiceEvents.update({
+          where: { id: lastVoiceEvent.id },
+          data: { leave: new Date() },
+        });
+      }
+
+      return;
+    }
+
+    // CHANGED VOICE CHANNEL
+    if (oldVoiceState.channelId !== newVoiceState.channelId) {
+      if (lastVoiceEvent?.leave === null) {
+        lastVoiceEvent = await daysBetween(data);
+        await prisma.guildVoiceEvents.update({
+          where: { id: lastVoiceEvent.id },
+          data: { leave: new Date() },
+        });
+      }
+
+      return await prisma.guildVoiceEvents.create({
+        data: { memberId, channelId, guildId },
+      });
+    }
+
+    return;
+  }
+
+  static async updateUserVoiceState(newVoiceState: VoiceState) {
+    if (!newVoiceState.channel) return;
+
+    await prisma.memberGuild.update({
+      where: {
+        member_guild: {
+          guildId: newVoiceState.guild.id,
+          memberId: newVoiceState.member!.id,
+        },
+      },
+      data: {
+        deafened: newVoiceState.serverDeaf || false,
+        muted: newVoiceState.serverMute || false,
+      },
+    });
+  }
+}
+
+async function daysBetween({
+  lastVoiceEvent,
+  memberId,
+  channelId,
+  guildId,
+}: {
+  memberId: string;
+  channelId: string;
+  guildId: string;
+  lastVoiceEvent: GuildVoiceEvents | null;
+}) {
+  if (!lastVoiceEvent?.join) return lastVoiceEvent as GuildVoiceEvents;
+
+  const now = new Date();
+  const daysBetween = getDaysArray(lastVoiceEvent.join, now);
+  if (daysBetween.length < 2) return lastVoiceEvent;
+
+  for (let i = 1; i < daysBetween.length; i++) {
+    await prisma.guildVoiceEvents.update({
+      where: { id: lastVoiceEvent.id },
+      data: {
+        leave: dayjs(lastVoiceEvent.join).utc().endOf("day").toDate(),
+      },
+    });
+    lastVoiceEvent = await prisma.guildVoiceEvents.create({
+      data: {
+        memberId,
+        channelId,
+        guildId,
+        join: dayjs(daysBetween[i]).utc().startOf("day").toDate(),
+      },
+    });
+  }
+  return lastVoiceEvent;
+}
