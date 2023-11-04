704bdb631dc8eea62b02b7b45a56059f3c2e5c64
don.cryptus <don.cryptus@gmail.com>
2023-11-04 23:06:58 +0100

    easy

--- src/types/enviroment.ts
+++ src/types/enviroment.ts
@@ -4,11 +4,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       TOKEN: string;
-      CLIENT_ID: string;
-      GUILD_ID: string;
-      MEGA_EMAIL: string;
-      MEGA_PASSWORD: string;
-      MEGA_DIR: string;
       DEEPL: string;
       OPEN_AI: string;
     }
