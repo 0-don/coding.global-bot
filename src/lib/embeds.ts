c0434ffbf8e2a57deb867df125cc026e9dc60e18
don.cryptus <don.cryptus@gmail.com>
2023-11-05 15:25:57 +0100

    easy

--- src/lib/embeds.ts
+++ src/lib/embeds.ts
@@ -55,7 +55,7 @@ export const topStatsExampleEmbed = ({
   const mostActiveMessageUsersString = mostActiveMessageUsers.map(
     ({ count, memberId, username }, i) =>
       `${codeString(placementSuffix(i + 1))} <@${memberId}>: (${username}) ${codeString(
-        count.toLocaleString("en") + " messages",
+        "helped" + count.toLocaleString("en") + " times",
       )}`,
   );
 
