a3cfb6ff7e023aefabf8f65d02676d71a8ddf173
don.cryptus <don.cryptus@gmail.com>
2023-11-11 17:27:18 +0100

    ok

--- src/chatgpt/askAi.ts
+++ src/chatgpt/askAi.ts
@@ -13,7 +13,7 @@ interface AskAi {
 }
 
 const MSG_LIMIT = 2000;
-const EDIT_THRESHOLD = 3;
+const EDIT_THRESHOLD = 5;
 
 export const askAi = async (props: AskAi) => {
   const memberGuild = await prisma.memberGuild.findFirst({
