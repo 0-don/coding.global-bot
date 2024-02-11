4de5f1e0b23f0807eaf08cdcde8a18f95146e8da
don.cryptus <don.cryptus@gmail.com>
2024-02-11 17:00:44 +0100

    x

--- src/events/MessageDelete.ts
+++ src/events/MessageDelete.ts
@@ -1,4 +1,3 @@
-import { AuditLogEvent } from "discord.js";
 import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
 import { MessagesService } from "../lib/messages/Messages.service.js";
@@ -8,9 +7,9 @@ export class MessageDelete {
   @On()
   async messageDelete([message]: ArgsOf<"messageDelete">, client: Client) {
     MessagesService.deleteMessageDb(message);
-    if (!message.guild) return; // Make sure this is not a DM
     
+    if (!message.guild) return; // Make sure this is not a DM
 
-    MessagesService.saveDeleteMessageHistory(message);
+    MessagesService.saveDeletedMessageHistory(message);
   }
 }
