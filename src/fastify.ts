ec965ab5009c2e7ed5344e51286d0d2ea6276a41
don.cryptus <don.cryptus@gmail.com>
2024-03-21 19:49:54 +0100

    update api

--- src/fastify.ts
+++ src/fastify.ts
@@ -1,94 +1,90 @@
-import cors from "@fastify/cors";
+import { serve } from "@hono/node-server";
+import dayjs from "dayjs";
 import { PermissionsBitField } from "discord.js";
-import Fastify from "fastify";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
 import { bot } from "./main.js";
-
-const fastify = Fastify({
-  // logger: true,
-});
-
-await fastify.register(cors, {
-  origin: ["*"],
-  credentials: true,
-});
 
 const cache: Record<string, any> = {};
 
-fastify.get("/api/:guildId/staff", async (req, reply) => {
-  const { guildId } = req.params as { guildId: string };
+const app = new Hono()
+  .use("/api/*", cors({ origin: "*" }))
+  .get("/api/:guildId/staff", async (c) => {
+    const { guildId } = c.req.param();
+    const cacheKey = `staff-${guildId}`;
 
-  const cacheKey = `staff-${guildId}`;
-  const currentTime = Date.now();
-
-  if (cache[cacheKey] && currentTime - cache[cacheKey].timestamp < 3600000000) {
-    return reply.send(cache[cacheKey].data);
-  }
-
-  const guild = bot.guilds.cache.get(guildId);
-  if (!guild) {
-    return reply.code(404).send({ error: "Guild not found" });
-  }
-
-  const members = (await guild.members.fetch())
-    .filter((member) =>
-      member.permissions.has(PermissionsBitField.Flags.MuteMembers)
-    )
-    .sort((a, b) => a.joinedAt!.getTime() - b.joinedAt!.getTime());
-
-  const staff: {
-    id: string;
-    username: string;
-    globalName: string | null;
-    joinedAt: string;
-    displayAvatarURL: string;
-    bannerUrl?: string | null;
-    displayHexColor: string;
-    memberRoles: string[];
-  }[] = [];
-
-  const userPromises = members.map((member) => member.user.fetch());
-
-  await Promise.all(userPromises);
-
-  for (const member of members.values()) {
-    if (member.user.bot) continue;
-    const memberRoles = member.roles.cache
-      .filter(
-        (role) =>
-          role.permissions.has(PermissionsBitField.Flags.MuteMembers) ||
-          role.permissions.has(PermissionsBitField.Flags.ManageMessages)
-      )
-      .map((role) => role.name);
-
-    await member.user.fetch();
-    if (memberRoles.length) {
-      staff.push({
-        id: member.id,
-        username: member.user.username,
-        globalName: member.user.globalName,
-        joinedAt: member.joinedAt!.toISOString(),
-        displayAvatarURL: member.user.displayAvatarURL({
-          size: 512,
-          extension: "webp",
-        }),
-        bannerUrl: member.user.bannerURL({ size: 512, extension: "webp" }),
-        displayHexColor: member.displayHexColor,
-        memberRoles,
-      });
+    if (
+      cache[cacheKey] &&
+      dayjs().diff(dayjs(cache[cacheKey].timestamp), "hour") < 1
+    ) {
+      return c.json(cache[cacheKey].data);
     }
-  }
 
-  cache[cacheKey] = {
-    timestamp: Date.now(),
-    data: staff,
-  };
+    const guild = bot.guilds.cache.get(guildId);
+    if (!guild) {
+      throw new HTTPException(401, { message: "Guild not found" });
+    }
 
-  reply.send(staff);
+    const members = (await guild.members.fetch())
+      .filter((member) =>
+        member.permissions.has(PermissionsBitField.Flags.MuteMembers)
+      )
+      .sort((a, b) => a.joinedAt!.getTime() - b.joinedAt!.getTime());
+
+    const staff: {
+      id: string;
+      username: string;
+      globalName: string | null;
+      joinedAt: string;
+      displayAvatarURL: string;
+      bannerUrl?: string | null;
+      displayHexColor: string;
+      memberRoles: string[];
+    }[] = [];
+
+    const userPromises = members.map((member) => member.user.fetch());
+
+    await Promise.all(userPromises);
+
+    for (const member of members.values()) {
+      if (member.user.bot) continue;
+      const memberRoles = member.roles.cache
+        .filter(
+          (role) =>
+            role.permissions.has(PermissionsBitField.Flags.MuteMembers) ||
+            role.permissions.has(PermissionsBitField.Flags.ManageMessages)
+        )
+        .map((role) => role.name);
+
+      if (memberRoles.length) {
+        staff.push({
+          id: member.id,
+          username: member.user.username,
+          globalName: member.user.globalName,
+          joinedAt: member.joinedAt!.toISOString(),
+          displayAvatarURL: member.user.displayAvatarURL({
+            size: 512,
+            extension: "webp",
+          }),
+          bannerUrl: member.user.bannerURL({ size: 512, extension: "webp" }),
+          displayHexColor: member.displayHexColor,
+          memberRoles,
+        });
+      }
+    }
+
+    cache[cacheKey] = {
+      timestamp: Date.now(),
+      data: staff,
+    };
+
+    return c.json(staff);
+  });
+
+serve({
+  fetch: app.fetch,
+  port: 3000,
 });
 
-fastify
-  .listen({
-    port: 3000,
-    host: process.env.NODE_ENV === "production" ? "0.0.0.0" : undefined,
-  })
-  .then((address) => console.log(`Server listening on ${address}`));
+console.log("Server started on port 3000");
