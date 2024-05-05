d4084c231191af043fdde7a89193337b72ddd559
KrishnaSSH <krishnasys@proton.me>
2024-05-05 21:32:04 +0530

    Update Translate.ts

--- src/commands/user/Translate.ts
+++ src/commands/user/Translate.ts
@@ -10,28 +10,28 @@ import { LogService } from "../../lib/logs/Log.service.js";
 export class Translate {
   @Slash({
     name: "translate",
-    description: "translate text to english",
+    description: "Translate text to English",
   })
   async translate(
     @SlashOption({
       name: "text",
-      description: "the text to translate",
+      description: "The text to translate",
       required: true,
       type: ApplicationCommandOptionType.String,
     })
     txt: string,
     interaction: CommandInteraction
   ) {
-    // deferReply if it takes longer then usual
+    // Defer reply if it takes longer than usual
     await interaction.deferReply();
     LogService.logCommandHistory(interaction, "translate");
 
-    // get lookback days from input
+    // Get lookback days from input
     const text = Buffer.from(txt, "utf-8").toString();
 
     const translatedText = await translate(text);
 
-    // send success message
+    // Send success message
     return await interaction.editReply({
       content: translatedText,
       allowedMentions: { users: [] },
