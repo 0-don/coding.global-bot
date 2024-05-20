f34d33cbb59b327f91f86b3d7d816d6e0ff3e29d
don.cryptus <don.cryptus@gmail.com>
2024-05-20 17:33:01 +0200

    easy

--- src/lib/roles/Roles.service.ts
+++ src/lib/roles/Roles.service.ts
@@ -48,8 +48,8 @@ export class RolesService {
           memberId: args.newMember.id,
           guildId: args.newMember.guild.id,
         };
-        try {
-          await prisma.memberRole.upsert({
+        prisma.memberRole
+          .upsert({
             where: {
               member_role: {
                 memberId: memberRole.memberId,
@@ -58,8 +58,8 @@ export class RolesService {
             },
             create: memberRole,
             update: memberRole,
-          });
-        } catch (_) {}
+          })
+          .catch(() => {});
       }
       // remove role
     } else if (args.newRoles.length < args.oldRoles.length) {
@@ -72,16 +72,16 @@ export class RolesService {
       if (!newRemovedRole) return;
 
       // try catch delete removed role from db
-      try {
-        await prisma.memberRole.delete({
+      prisma.memberRole
+        .delete({
           where: {
             member_role: {
               memberId: args.newMember.id,
               roleId: newRemovedRole.id,
             },
           },
-        });
-      } catch (_) {}
+        })
+        .catch(() => {});
     }
   }
 
