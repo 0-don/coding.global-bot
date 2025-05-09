6ed77aefb60951f44139b9c0199b4b5f4da8c7f4
0-don <70039285+0-don@users.noreply.github.com>
2025-05-09 18:29:43 +0200

    Update dependencies in package.json and refactor MessageCreate class methods for improved clarity and structure

--- src/events/JobCreate.ts
+++ src/events/JobCreate.ts
@@ -1,27 +1,25 @@
 import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
-import { JOB_POST_REGEX } from "../lib/helpers.js";
 import { DEV_BOARD_CHANNEL } from "../lib/constants.js";
+import { JOB_POST_REGEX } from "../lib/helpers.js";
 
 @Discord()
 export class MessageCreate {
-    private readonly JOB_CHANNEL_ID = DEV_BOARD_CHANNEL;
+  @On({ event: "messageCreate" })
+  async onMessage([message]: ArgsOf<"messageCreate">, client: Client) {
+    if (message.channelId !== DEV_BOARD_CHANNEL) return;
+    if (message.author.bot) return;
 
-    @On({ event: "messageCreate" })
-    async onMessage([message]: ArgsOf<"messageCreate">, client: Client) {
-        if (message.channelId !== this.JOB_CHANNEL_ID) return;
-        if (message.author.bot) return;
-        
-        if (!JOB_POST_REGEX.test(message.content)) {
-            await message.delete();
-            
-            try {
-                await message.author.send({
-                    content: `Your post in the job board was deleted because it didn't follow the required format`
-                });
-            } catch {
-                console.log(`Couldn't send DM to ${message.author.tag}`);
-            }
-        }
+    if (!JOB_POST_REGEX.test(message.content)) {
+      await message.delete();
+
+      try {
+        await message.author.send({
+          content: `Your post in the job board was deleted because it didn't follow the required format`,
+        });
+      } catch {
+        console.log(`Couldn't send DM to ${message.author.tag}`);
+      }
     }
+  }
 }
