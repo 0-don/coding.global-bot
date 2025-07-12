4b40c3a29a0d130c2e10357a27189bb3bde36d87
spectre <alexandertejedadlr@gmail.com>
2025-07-12 14:50:57 -0400

    Update ai.ts

--- src/events/AI/ai.ts
+++ src/events/AI/ai.ts
@@ -66,7 +66,7 @@ export class AIChat {
       });
 
       const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash :generateContent?key=${apiKey}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
