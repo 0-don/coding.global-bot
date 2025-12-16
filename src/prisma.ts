c73a831e8d4e0fe0f30a04a5d21b4ca9ec0df50c
0-don <don.cryptus@gmail.com>
2025-12-17 00:46:57 +0100

    refactor: streamline Prisma adapter initialization in prisma.ts

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 export const prisma = new PrismaClient({ adapter });
